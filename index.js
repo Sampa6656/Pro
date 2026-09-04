f1cfca1bb7dd11239649a922d87236fa76750a2a
Matheus Gustavo <sampaaa3as@gmail.com>
2026-09-04 01:17:24 -0300

    Ajusta argumentos finais para contornar segfault do cloudways

--- index.js
+++ index.js
@@ -124,16 +124,13 @@ function criarClienteWhatsApp(idSessao) {
     const client = new Client({
         authStrategy: new LocalAuth({ clientId: idSessao }),
         puppeteer: { 
-            headless: 'new',
-            executablePath: undefined,
+            executablePath: '/usr/bin/chromium-browser',
+            headless: true,
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--no-zygote',
-                '--deterministic-fetch',
-                '--disable-features=IsolateOrigins,site-per-process'
+                '--disable-gpu'
             ]
         }
     });
